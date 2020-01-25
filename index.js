ef14628b871f6587e1c8082fdaaa2481fa261661
Eric (Martin) Mancini <coelomate@gmail.com>
2020-01-25 14:14:23 -0500

    working server

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let lightStatus = {
+  liana: false,
+  eric: false
+};
+
+let piMatches = {
+  liana: 'eric',
+  eric: 'liana'
+};
+
 app.get('/', (req, res) => {
   res.send(`
   <html>
     <h1>Pi Flowers</h1>
     <hr />
     <h2>Liana Flower</h2>
+    <p><strong>Lit: </strong>(${lightStatus.liana})</p>
     <ul>
       <li><a href="/button?pi=liana">Press Button</a></li>
       <li><a href="/light?pi=liana">Should I light up?</a></li>
     </ul>
     <h2>Eric Flower</h2>
+    <p><strong>Lit: </strong>(${lightStatus.eric})</p>
     <ul>
       <li><a href="/button?pi=eric">Press Button</a></li>
       <li><a href="/light?pi=eric">Should I light up?</a></li>
@@ -23,14 +35,33 @@ app.get('/', (req, res) => {
   `);
 });
 
+// Determine if the pi should light up, then reset status to false
 app.get('/light', (req, res) => {
-  console.log(req.query.pi);
-  res.json({success: 'test', route: 'light', requester: req.query.pi});
+  let requestingPi = req.query.pi;
+  console.log(`light poll received from ${requestingPi}`);
+
+  let shouldLight = lightStatus[requestingPi];
+  lightStatus[requestingPi] = false;
+
+  res.json({
+    route: 'light',
+    requester: requestingPi,
+    shouldLight: shouldLight
+  });
 });
 
+// Tell the other pi to light up the next time it polls /light
 app.get('/button', (req, res) => {
-  console.log(req.query.pi);
-  res.json({success: 'test', route: 'button', requester: req.query.pi});
+  let requestingPi = req.query.pi;
+  console.log(`button press received from ${requestingPi}`);
+
+  // Set the OTHER pi's lightStatus to true
+  lightStatus[piMatches[requestingPi]] = true;
+
+  res.json({
+    route: 'button',
+    requester: requestingPi
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
